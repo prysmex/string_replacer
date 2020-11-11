f6c0f75579697a34d65516df5622d7c349a87a90
Pato <pato_devilla@hotmail.com>
2020-11-10 22:17:21 -0600

    first commit

---
+++ lib/string_replacer.rb
@@ -0,0 +1,6 @@
+require "string_replacer/version"
+
+module StringReplacer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/string_replacer/version.rb
@@ -0,0 +1,3 @@
+module StringReplacer
+  VERSION = "0.1.0"
+end
