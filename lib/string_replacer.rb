5afd3db9acd234e034b60155295607ab363ce4e3
Pato <pato_devilla@hotmail.com>
2020-11-10 22:19:05 -0600

    add string replacer class

--- lib/string_replacer.rb
+++ lib/string_replacer.rb
@@ -1,6 +1,43 @@
 require "string_replacer/version"
 
-module StringReplacer
-  class Error < StandardError; end
-  # Your code goes here...
+class StringReplacer
+
+  HANDLEBARS_REGEX = /{{.*?}}/
+  INNERMOST_METHOD_REGEX = /{{.*?\b((\w+)\(((?=[^(]*?\)).*?)\))[) ]*}}/
+
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def replace
+    string = @string
+    string.scan(HANDLEBARS_REGEX).each do |handlebars|
+      result = execute_methods(handlebars)
+      result = result.slice(2..-3) #remove the handlebars
+      string = string.sub(handlebars, result)
+    end
+    return string
+  end
+  
+  def execute_methods(handlebars)
+    string = handlebars
+
+    match = string.match(INNERMOST_METHOD_REGEX)
+    if match
+      captures = match.captures
+      to_replace = captures[0]
+      method_name = captures[1]
+      argument = captures[2]
+      if self.respond_to?(method_name)
+        result = self.public_send(method_name, argument)
+        string = string.sub(to_replace, result)
+        string = execute_methods(string)
+      end
+    end
+
+    return string
+  end
+
 end
